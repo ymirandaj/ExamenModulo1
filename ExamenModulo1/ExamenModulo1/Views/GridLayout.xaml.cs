e7e532648cb8953c8ce8e496d847b5b3a8cfd710
Yomar Miranda <yomarmiranda16@gmail.com>
2020-09-07 10:11:48 -0500

    Agregar archivos de proyecto.

---
+++ ExamenModulo1/ExamenModulo1/Views/GridLayout.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExamenModulo1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GridLayout : ContentPage
+    {
+        public GridLayout()
+        {
+            InitializeComponent();
+ 
+        }
+    }
+}
